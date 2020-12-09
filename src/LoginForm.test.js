daa5266f0ca187da762f3fb5c72af33f785522e7
Luis Takahashi <takahashihideki408@gmail.com>
2020-12-08 23:23:42 -0300

    Added solution

--- src/LoginForm.test.js
+++ src/LoginForm.test.js
@@ -2,16 +2,39 @@ import { render, screen } from '@testing-library/react';
 import user from '@testing-library/user-event';
 import { LoginForm } from './LoginForm';
 
+import * as api from './api';
+
+jest.spyOn(api, 'login');
+
+afterEach(() => {
+  api.login.mockReset();
+})
+
+afterAll(() => {
+  api.login.mockRestore();
+})
+
 describe('LoginForm', () => {
   it('should allow a user to log in', async () => {
+    const username = 'takahashi';
+    const mockedUser = {
+      id: '9364ecde-6f81-4395-82a2-2c4317ea4fb6',
+      name: 'Some other name',
+      lastName: 'A bit more resilient test'
+    }
+    api.login.mockResolvedValue(mockedUser);
+
     render(<LoginForm />)
 
-    user.type(screen.getByLabelText(/username/i), 'Takahashi')
+    user.type(screen.getByLabelText(/username/i), username)
 
     user.click(screen.getByRole('button', { name: /submit/i }))
 
-    expect(await screen.findByText('9364ecde-6f81-4395-82a2-2c4317ea4fb6')).toBeInTheDocument()
-    expect(await screen.findByText('Taka')).toBeInTheDocument()
-    expect(await screen.findByText('Hashi')).toBeInTheDocument()
+    expect(await screen.findByText(mockedUser.id)).toBeInTheDocument()
+    expect(await screen.findByText(mockedUser.name)).toBeInTheDocument()
+    expect(await screen.findByText(mockedUser.lastName)).toBeInTheDocument()
+
+    expect(api.login).toHaveBeenCalledWith(username);
+    expect(api.login).toHaveBeenCalledTimes(1)
   })
 })
